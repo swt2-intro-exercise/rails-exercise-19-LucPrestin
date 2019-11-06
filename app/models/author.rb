5b31e54398f1de406830d75faf9250b8ef409347
Luc Prestin <l.prestin@gmx.de>
2019-11-06 11:25:01 +0100

    added validation for authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,7 @@
 class Author < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :homepage, presence: true
 
     def name
         self.first_name + ' ' + self.last_name
