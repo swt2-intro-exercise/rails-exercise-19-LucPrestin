10632f3fca6649470b60208b5cf5900b9810c7cb
Luc Prestin <l.prestin@gmx.de>
2019-11-07 10:12:48 +0100

    added 'has_and_belongs_to_many'-relation to authors and papers

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,8 @@ class Author < ApplicationRecord
     validates :last_name, presence: true
     validates :homepage, presence: true
 
+    has_and_belongs_to_many :papers
+
     def name
         self.first_name + ' ' + self.last_name
     end
