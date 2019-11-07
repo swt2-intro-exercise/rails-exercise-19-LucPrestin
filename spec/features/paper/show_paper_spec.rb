7dd4177b329ea6f14cfbb0e9365d7b8f223fbdf8
Luc Prestin <l.prestin@gmx.de>
2019-11-07 10:18:06 +0100

    Failing test for #14

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -11,4 +11,13 @@ describe "Show paper page", type: :feature do
         expect(page).to have_text('Year: 1950')
     end
 
+    it "should display a paper's authors" do
+        @author = FactoryBot.create :author
+        @paper = FactoryBot.create :paper
+
+        visit paper_path(@paper)
+
+        expect(page).to have_text('Alan Turing')
+    end
+
 end
